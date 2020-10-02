3f333120bc66418a00c31fc167ebf67f67e96667
William Cragg <wstc@riseup.net>
2020-10-02 18:29:52 +0100

    added professional experience from CV

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 constructor() {
   super();
   this.state = {
-    route: 'home',
+    route: 'portfolio',
   }
   }
 
@@ -35,8 +35,37 @@ onRouteChange = (route) => {
           <h2>Human Skills</h2><p>Workshop facilitation, Design thinking, Conflict resolution, Free-association narrative interviews, open, honest and direct.</p>
           <h2>Interests</h2><p>Federeated Learning, Baysian Inference, Data Fusion, DLTs, Design Theory, Generative Art, Multi-Agent Systems</p>
           </div> 
-        : ( route === 'portfolio' ? <div><PortfolioItem title={'arrk'} /></div> 
-        : ( route === 'contact' ? <div><p>contact</p></div> : <div><p>home</p></div> 
+        : ( route === 'portfolio' ? 
+          <div>
+                  <PortfolioItem date={'October 2019-present'} 
+                    title={'Tree Tents International'} 
+                    location={'Brighton'} 
+                    body={['Core responsibility is to help the company simplify business processes and increase manufacturing capacity. Also assisting in product design and assembly. Achievements include:',
+                    <ul>
+                      <li>Identified, installed and adapted open source MRP/CRM software to simplify operations.</li>
+                      <li>Installed host server and facilitated transition to new system.</li>
+                    </ul>]} />
+          <PortfolioItem date={'May 2017-September 2019'} 
+                    title={'Arrk Group'} 
+                    location={'Manchester'} 
+                    body={['Software design, development and data analysis. Product research and business strategy. Swift, Kotlin, Python, SQL, PowerBI, AWS, Jenkins, JIRA. Achievements include:',
+                     <ul>
+                     <li>Instrumental in the design, delivery and continual development of a B2C lifestyle and finance platform, currently with 900,000+ users. Worked closely with two others to win the contract for this project worth several million pounds.</li>
+                     <li>Helped design and implement a platform-wide analytics and data strategy.</li>
+                     <li>Constantly collaborated with a geographically distributed and cross-cultural team. Favoured openness and encouraged a culture of consensus decision making.</li>
+                     <li>Incorporated ethnographic research and user testing into a rapid value proposition design process. Used a clear, confident presentation style to update key stakeholders on a weekly basis and build trust in my team's methods.</li>
+                     <li>Learnt new programming languages on the job and produced production-ready code - often working on multiple platforms at once.</li>
+                     <li>Facilitated regular workshops with customers and staff - creating a space for collaboration and active participation with a sensitivity towards the needs of individual participants.</li>
+                     </ul>]} />
+                              <PortfolioItem date={'Jan--March 2017'} 
+                    title={'Brighton \& Hove Energy Services'} 
+                    location={'Brighton'} 
+                    body={['Graduate intern responsible for lead generation, researching and drafting project proposals, maintaining financial models in Excel, coordinating and invoicing contractors, analysis and communication of energy generation data.']} />
+                     </div>
+
+
+
+        : ( route === 'contact' ? <div><p>contact</p></div> : this.setState({route: 'home'}) 
         )))
       }
       </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,12 @@
 import React from 'react';
 // className="pa2 ma2 bg-green" variant='primary'
-export const PortfolioItem = ({ title }) => {
+export const PortfolioItem = ({ date, title, location, body }) => {
   return (
 			<div>
+		<h3>{date}</h3>
 		<h2>{title}</h2>
+		<h3>{location}</h3>
+		<p>{body}</p>
 
 		</div>
     )
