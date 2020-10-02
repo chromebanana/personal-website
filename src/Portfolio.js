68738b94cf02f6038a18742baa9740be61f49b69
William Cragg <wstc@riseup.net>
2020-10-02 17:16:48 +0100

    rearange

---
+++ src/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// className="pa2 ma2 bg-green" variant='primary'
+export const PortfolioItem = ({ title }) => {
+  return (
+			<div>
+		<h2>{title}</h2>
+
+		</div>
+    )
+}
