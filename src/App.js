c1a421b27ab7ce0960f4b1c0285d9feb70561dd0
William Cragg <wstc@riseup.net>
2020-10-05 22:47:46 +0100

    styling of portfolio

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ onRouteChange = (route) => {
  // TODO: https://en.wikipedia.org/wiki/Centrifugal_governor
   return (
     <div className="App">
-    <Navigation onRouteChange={this.onRouteChange}/>
+
+    <div className="mw7 ph3 center">
+        <Navigation onRouteChange={this.onRouteChange}/>
+
       {this.state.route === 'home' ?
         <div>
         <h1>William Cragg</h1>
@@ -30,10 +33,10 @@ onRouteChange = (route) => {
         </div>
         : ( route === 'bio' ? <div>
           <h2>About Me</h2><p>
-          Hard-working team player motivated by technology's capacity to accelerate social and environmental change. Physicist with diverse experience in software design and full stack development. Strong qualitative and quantitative research tool-set. Mostly using Python and React. I also human very well.</p>
-          <h2>Tools & Technical Skills</h2><p>React, Python, Swift, Data Analysis, AWS, Smart Contracts, Machine Learning, Embedded Software</p>
+          Enthusiastic team player motivated by technology's capacity to accelerate social and environmental change. Physicist with diverse experience in software design and full stack development. Strong qualitative and quantitative research tool-set. Mostly using Python and React. I also human very well.</p>
+          <h2>Tools & Technical Skills</h2><p>React, Python, Swift, Data Analysis, AWS, Smart Contracts, Machine Learning, Embedded Software, Control Systems</p>
           <h2>Human Skills</h2><p>Workshop facilitation, Design thinking, Conflict resolution, Free-association narrative interviews, open, honest and direct.</p>
-          <h2>Interests</h2><p>Federeated Learning, Baysian Inference, Data Fusion, DLTs, Design Theory, Generative Art, Multi-Agent Systems</p>
+          <h2>Interests</h2><p>Federeated Learning, Baysian Inference, Data Fusion, DLTs, Design Theory, Generative Art, Multi-Agent Systems, Wallace & Gromit</p>
           </div> 
         : ( route === 'portfolio' ? 
           <div>
@@ -65,11 +68,13 @@ onRouteChange = (route) => {
 
 
 
-        : ( route === 'contact' ? <div><p>contact</p></div> : this.setState({route: 'home'}) 
+        : ( route === 'contact' ? <div><p>contact id also love feed back on this website/any usabiltiy issues responsiveness</p></div> : this.setState({route: 'home'}) 
         )))
       }
+      </div> 
       </div>
   );
+
 }
 }
 
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,12 +2,14 @@ import React from 'react';
 // className="pa2 ma2 bg-green" variant='primary'
 export const PortfolioItem = ({ date, title, location, body }) => {
   return (
-			<div>
+  		<div className="pv2">
+  		<article className="ba ph4 pv2">
 		<h3>{date}</h3>
 		<h2>{title}</h2>
 		<h3>{location}</h3>
-		<p>{body}</p>
+		<p className="">{body}</p>
+		</article>	
+  		</div>
 
-		</div>
     )
 }
