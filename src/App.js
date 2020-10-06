9f00e82481dfdc803a25628ccede9e6aec7c5806
William Cragg <wstc@riseup.net>
2020-10-06 13:05:28 +0100

    updated proffesional experience

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navigation } from './Navigation';
-import { PortfolioItem } from './Portfolio';
+import { Portfolio } from './Portfolio';
 
 import './App.css';
 
@@ -8,7 +8,7 @@ class App extends Component {
 constructor() {
   super();
   this.state = {
-    route: 'bio',
+    route: 'portfolio',
   }
   }
 
@@ -35,49 +35,24 @@ onRouteChange = (route) => {
           <h2>About Me</h2><p>
           Enthusiastic team player pursuing a career at the intersection of AI and sustainability.
           I'm motivated by technology's capacity to accelerate social and environmental change. 
-          I'd like to work in a high performance organisation on important difficult problems. 
+          I'd like to work in a high performance organisation tackling challenging, critical problems. 
           A job that allows me to flex my mathematics muscles is a must💪.
-           <strong> I'm particularly interested in working on industrial scale applications of privacy-preserving machine learning and data fusion.</strong> 
+           <strong> I'm particularly interested in working on industrial scale applications of privacy-preserving machine learning, data fusion or multi-agent modelling.</strong> 
           <ul>
-            <li>I'm a physicist with diverse experience in all aspects of software production.</li>
+            <li>I'm a physicist with experience in all aspects of software production.</li>
             <li>Strong qualitative and quantitative research tool-set.</li>
             <li>Thorough, diligent and thoughtful.</li>
             <li>Mostly using Python and React.</li>
           </ul>
           I also human very well.</p>
           <h2>Tools & Technical Skills</h2>
-          <p className="mb1">With Proffesional Experience: React, Python, Swift, SQL, Data Analysis, AWS.</p>
-          <p className="mt0">Academic and hobby: C, Machine Learning, Smart Contracts, 32-bit microcontrollers.</p>
-          <h2>Human Skills</h2><p>Workshop facilitation, Design thinking, Free-association narrative interviews, clear and confident presentation style, open, honest and direct.</p>
-          <h2>Relevant Interests</h2><p>Federeated Learning, Baysian Inference, Data Fusion, DLTs, Design Theory, Generative Art, Multi-Agent Systems, Non-violent Communication, Wallace & Gromit</p>
+          <p className="mb1">With Proffesional Experience: Python, React, Swift, SQL, Data Analysis and Reporting, AWS.</p>
+          <p className="mt0">Academic and hobby: C, C++, Machine Learning, Smart Contracts, 32-bit microcontrollers, TouchDesigner, Processing.</p>
+          <h2>Human Skills</h2><p>Workshop facilitation, Design thinking, Free-association narrative interviews, clear and confident presentation style, kind, honest and direct.</p>
+          <h2>Relevant Interests</h2><p>Federeated Learning, Data Fusion using Baysian Inference, DLTs, Design Theory, Generative Art, Multi-Agent Systems, Power Dynamics, Non-violent Communication, Wallace & Gromit</p>
           </div> 
         : ( route === 'portfolio' ? 
-          <div>
-                  <PortfolioItem date={'October 2019-present'} 
-                    title={'Tree Tents International'} 
-                    location={'Brighton'} 
-                    body={['Core responsibility is to help the company simplify business processes and increase manufacturing capacity. Also assisting in product design and assembly. Achievements include:',
-                    <ul>
-                      <li>Identified, installed and adapted open source MRP/CRM software to simplify operations.</li>
-                      <li>Installed host server and facilitated transition to new system.</li>
-                    </ul>]} />
-          <PortfolioItem date={'May 2017-September 2019'} 
-                    title={'Arrk Group'} 
-                    location={'Manchester'} 
-                    body={['Software design, development and data analysis. Product research and business strategy. Swift, Kotlin, Python, SQL, PowerBI, AWS, CircleCI, JIRA. Achievements include:',
-                     <ul>
-                     <li>Instrumental in the design, delivery and continual development of a B2C lifestyle and finance platform, currently with 900,000+ users. Worked closely with two others to win the contract for this project worth several million pounds.</li>
-                     <li>Helped design and implement a platform-wide analytics and data strategy.</li>
-                     <li>Constantly collaborated with a geographically distributed and cross-cultural team. Favoured openness and encouraged a culture of consensus decision making.</li>
-                     <li>Incorporated ethnographic research and user testing into a rapid value proposition design process. Used a clear, confident presentation style to update key stakeholders on a weekly basis and build trust in my team's methods.</li>
-                     <li>Learnt new programming languages on the job and produced production-ready code - often working on multiple platforms at once.</li>
-                     <li>Facilitated regular workshops with customers and staff - creating a space for collaboration and active participation with a sensitivity towards the needs of individual participants.</li>
-                     </ul>]} />
-                              <PortfolioItem date={'Jan--March 2017'} 
-                    title={'Brighton \& Hove Energy Services'} 
-                    location={'Brighton'} 
-                    body={['Graduate intern responsible for lead generation, researching and drafting project proposals, maintaining financial models in Excel, coordinating and invoicing contractors, analysis and communication of energy generation data.']} />
-                     </div>
+          <Portfolio/>
 
 
 
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,15 +1,60 @@
 import React from 'react';
 // className="pa2 ma2 bg-green" variant='primary'
-export const PortfolioItem = ({ date, title, location, body }) => {
+ const PortfolioItem = ({ date, role, title, location, body }) => {
   return (
   		<div className="pv2">
   		<article className="ba ph4 pv2">
 		<h3>{date}</h3>
+		<h3>{role}</h3>
 		<h2>{title}</h2>
 		<h3>{location}</h3>
 		<p className="">{body}</p>
 		</article>	
   		</div>
+
+    )
+}
+
+export const Portfolio = ({ }) => {
+  return (
+          <div>
+	      <PortfolioItem date={'October 2019-present'} 
+	      	role={'Production Supervisor, Developer'}
+	        title={'Tree Tents International'} 
+	        location={'Brighton'} 
+	        body={[<p>Core responsibility is to help the company simplify business processes and increase manufacturing capacity. Also assisting in product design and assembly. <strong>React, Python, Odoo, Ubuntu-server, Trello.</strong></p>,
+	        <ul>
+	          <li>Identified, installed and adapted open source MRP/CRM software to simplify operations.</li>
+	          <li>Installed host server and facilitated transition to new system.</li>
+	          <li>Developing front-end sales and warehouse tools to interface with MRP system.</li>
+	        </ul>]} />
+          <PortfolioItem date={'May 2017-September 2019'} 
+          	role={'Consultant, Developer'}
+            title={'Arrk Group'} 
+            location={'Manchester'} 
+            body={[<p>Software design, development and data analysis. Product research and business strategy. <strong>Swift, Kotlin, Python, SQL, AWS, PowerBI, CircleCI, JIRA.</strong> Achievements include:</p>,
+             <ul>
+             <li>Instrumental in the design, delivery and continual development of a B2C lifestyle and finance platform, currently with 900,000+ users. Worked closely with two others to win the contract for this project worth several million pounds.</li>
+             <li>Helped design and implement a platform-wide analytics and data strategy.</li>
+             <li>Constantly collaborated with a geographically distributed and cross-cultural team. Favoured openness and encouraged a culture of consensus decision making.</li>
+             <li>Incorporated ethnographic research and user testing into a rapid value proposition design process. Used a clear, confident presentation style to update key stakeholders on a weekly basis and build trust in my team's methods.</li>
+             <li>Learnt new programming languages on the job and produced production-ready code - often working on multiple platforms at once.</li>
+             <li>Facilitated regular workshops with customers and staff - creating a space for collaboration and active participation with a sensitivity towards individual needs.</li>
+             </ul>]} />
+	      <PortfolioItem date={'Jan--March 2017'}
+	      	role={'Technical Project Coordinator'} 
+            title={'Brighton \& Hove Energy Services'} 
+            location={'Brighton'} 
+            body={[<p>Graduate internship.<strong> Excel, CiviCRM, Python, GIMP.</strong>  Achievements and responsibilies included:</p>,
+            <ul>
+            	<li>Researched and drafted project proposals.</li>
+            	<li>Maintained financial models.</li>
+            	<li>Coordinated contractors.</li>
+            	<li>Analysed and reported energy generation data.</li>
+            	<li>Secured a customer and delivered an energy saving project.</li>
+            	<li>Produced customer and public facing infographics to communicate energy savings and investment value.</li>
+            </ul>]} />
+         </div>
 
     )
 }
