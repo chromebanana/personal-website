3b8113a290b5fc1ad4c34062fd82667ee89c2fc7
William Cragg <wstc@riseup.net>
2020-10-06 18:32:58 +0100

    added menu buttons for portfolio section, a qualifications section and updated contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navigation } from './Navigation';
-import { Portfolio } from './Portfolio';
+import Portfolio from './Portfolio';
 
 import './App.css';
 
@@ -29,13 +29,13 @@ onRouteChange = (route) => {
       {this.state.route === 'home' ?
         <div>
         <h1>William Cragg</h1>
-        <h2>Full Stack Developer</h2>
+        <h2>Full Stack Developer.  LAST COMMITTTT</h2>
         </div>
         : ( route === 'bio' ? <div>
           <h2>About Me</h2><p>
           Enthusiastic team player pursuing a career at the intersection of AI and sustainability.
           I'm motivated by technology's capacity to accelerate social and environmental change. 
-          I'd like to work in a high performance organisation tackling challenging, critical problems. 
+          I'd like to work in a high performance organisation focused on solving societally important problems. 
           A job that allows me to flex my mathematics muscles is a must💪.
            <strong> I'm particularly interested in working on industrial scale applications of privacy-preserving machine learning, data fusion or multi-agent modelling.</strong> 
           <ul>
@@ -44,19 +44,19 @@ onRouteChange = (route) => {
             <li>Thorough, diligent and thoughtful.</li>
             <li>Mostly using Python and React.</li>
           </ul>
-          I also human very well.</p>
+          </p>
           <h2>Tools & Technical Skills</h2>
           <p className="mb1">With Proffesional Experience: Python, React, Swift, SQL, Data Analysis and Reporting, AWS.</p>
           <p className="mt0">Academic and hobby: C, C++, Machine Learning, Smart Contracts, 32-bit microcontrollers, TouchDesigner, Processing.</p>
           <h2>Human Skills</h2><p>Workshop facilitation, Design thinking, Free-association narrative interviews, clear and confident presentation style, kind, honest and direct.</p>
-          <h2>Relevant Interests</h2><p>Federeated Learning, Data Fusion using Baysian Inference, DLTs, Design Theory, Generative Art, Multi-Agent Systems, Power Dynamics, Non-violent Communication, Wallace & Gromit</p>
+          <h2>Relevant Interests</h2><p>Federeated Learning, Data Fusion using Baysian Inference, DLTs, Design Theory, Generative Art, Multi-Agent Systems, Power Dynamics, Non-violent Communication, Wallace & Gromit.</p>
           </div> 
         : ( route === 'portfolio' ? 
           <Portfolio/>
 
 
 
-        : ( route === 'contact' ? <div><p>contact id also love feed back on this website/any usabiltiy issues responsiveness</p></div> : this.setState({route: 'home'}) 
+        : ( route === 'contact' ? <div><p>Open to Jobs, collaborations, art projects, or just conversation. I'd love feedback on this website, particularly regarding responsiveness or browser support. Im currently building the backend for a 'contact form' using Django. If you think this is the wrong thing to use I'd like to know why! INCLUDE BUTTONS FOR GITHUB TWITTER LINKEDIN</p></div> : this.setState({route: 'home'}) 
         )))
       }
       </div> 
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// className="pa2 ma2 bg-green" variant='primary'
- const PortfolioItem = ({ date, role, title, location, body }) => {
+ const PortfolioItem = ({ date, role, title, location, detail, body }) => {
   return (
   		<div className="pv2">
   		<article className="ba ph4 pv2">
@@ -8,6 +7,7 @@ import React from 'react';
 		<h3>{role}</h3>
 		<h2>{title}</h2>
 		<h3>{location}</h3>
+		<h4>{detail}</h4>
 		<p className="">{body}</p>
 		</article>	
   		</div>
@@ -15,9 +15,40 @@ import React from 'react';
     )
 }
 
-export const Portfolio = ({ }) => {
-  return (
+
+const Button = ({ currentSelection, text, onClick }) => {
+		console.log('test' + {currentSelection})
+
+	// console.log({currentSelection}==='Proffesional')
+  return(
+ <a class={{currentSelection}==={text} ? "f6 link dim ph3 pv2 mb2 dib white bg-black" : "f6 link dim ph3 pv2 mb2 dib white bg-black"} onClick={onClick} href="#0">{text}</a>
+  )
+}
+
+
+class Portfolio extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			category: 'Qualifications'
+		}
+	}
+
+	changePortfolioCategory = (event) => {
+		let target = event.target
+		console.log(target.text)
+		this.setState({category: target.text})
+		console.log(this.state.category)
+
+	}
+
+ render() {
+ 	return (
           <div>
+          <Button 
+          currentSelection={this.state.category} text={'Proffesional'} onClick={this.changePortfolioCategory}/><Button currentSelection={this.state.category} text={'Qualifications'} onClick={this.changePortfolioCategory}/>
+	      {this.state.category === 'Proffesional' ?
+	      <div>
 	      <PortfolioItem date={'October 2019-present'} 
 	      	role={'Production Supervisor, Developer'}
 	        title={'Tree Tents International'} 
@@ -54,7 +85,62 @@ export const Portfolio = ({ }) => {
             	<li>Secured a customer and delivered an energy saving project.</li>
             	<li>Produced customer and public facing infographics to communicate energy savings and investment value.</li>
             </ul>]} />
+           </div> : <div>
+           <PortfolioItem date={'In Progress'} 
+          	
+            title={'Machine Learning by Stanford University'} 
+            location={'Coursera, Online'} 
+            body={[<p>Currently enjoying Andrew Ng's Machine Learning course on Coursera. Submitting my assignments in Python using Jupyter notebook thanks you @@@@ for converting the coursework! Taking my time to comprehend and check proofs. I've missed writing equations.</p>,
+             <ul className="list">
+             <li>✅ Week 1: Linear Regression with one variable</li>
+             <li>✅ Week 2: Linear Regression with Multiple Variables</li>
+             <li>✅ Week 3: Logistic Regression, Regularisation</li>
+             <li>✅ Week 4: Neural Networks Intro </li>
+             <li>✅ Week 5: Training Neural Networks</li>
+             <li>📚 Week 6: Debugging, Duiagnostics, System Design</li>
+             <li>⬜️ Week 7: Support Vector Machines</li>
+             <li>⬜️ Week 8: Unsupervised Learning, Dimensionality Reduction</li>
+             <li>⬜️ Week 9: Anomaly Detection, Recommender Systems</li>
+             <li>⬜️ Week 10: Large Scale Machine Learning</li>
+             <li>⬜️ Week 11: Application Example: Photo OCR</li>
+
+             </ul>]} />
+            <PortfolioItem date={'June-August 2018'} 
+            title={'Blockchain Developer Program 2018'} 
+            location={'Consensys Academy, Online'} 
+            detail={'Grade: 76.1'}
+            body={<p>Smart Contract Development and Ethereum fundamentals. The course introduced key tools, security best practises and other aspects of dApp development. <a href="https://github.com/chromebanana/battleships-ethereum">My submission</a> enabled play (and betting) of turn based games using the Ethereum blockchain. My Smart Contract was deployed to the Ropsten Test Network.</p>
+             } />
+             <PortfolioItem date={'2012-2018'} 
+            title={'Master of Physics'} 
+            location={'University of Sussex'} 
+            detail={'First Class, MPhys (Hons).'}
+            body={<ul>
+            	<li>Received faculty award for 'Outstanding Improvement' in year 2.</li>
+            	<li>Excelled in the complex mathematics required by theoretical modules.</li>
+            	<li>Routinely used Python for data analysis. Also sat a C++ module to study OOP.</li>
+            	<li>Final year project was to design and build a PID control system for a laser: Circuit building, 32-bit microcontroller programming in C and creating a UI in Labview.</li>
+            	<li>Developed solid experimental and analytical skills and a ruthless attention to detail.</li>
+            	<li>Produced eloquently written reports in Latex. Awarded 95% for final year presentation.</li>
+            	<li>Became adept at identifying relevant physical principles involved in a problem and employing mathematical techniques to reach a clear solution.</li>
+            	<li>Audited modules on HCI and web development.</li>
+            	</ul>
+             } />
+             <PortfolioItem date={'2011-2012'} 
+            title={'Physics Foundation Year'} 
+            location={'University of Manchester'} 
+            detail={'71.25% Average'}
+             />
+             <PortfolioItem date={'Jan-September 2011'} 
+            title={'Polymaths - Certificate of the Institute of Mathematics and its Applications'} 
+            location={'University of Bolton'} 
+            
+             />
+             </div> }
          </div>
 
-    )
+    );
+ } 
 }
+
+export default Portfolio;
